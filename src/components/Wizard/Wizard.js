af467d58ec1c79b7cbdf03d1e941574d189685bf
Alma Powell <alma111000@gmail.comgit config --global user.email alma111000@gmail.comgit config --global user.name Alma>
2019-06-14 11:49:11 -0600

    step 1

---
+++ src/components/Wizard/Wizard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Wizard extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Wizard</h1>
+
+            </div>
+        )
+    }
+}
